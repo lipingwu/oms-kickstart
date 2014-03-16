4b64a135e500ef840c17daeeedc4f74be83e69c6
luminous <luminaries@riseup.now>
2014-03-16 23:55:11 +0000

    t2140: update kick-tmux.sh to install tmux

--- kick-tmux.sh
+++ kick-tmux.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 PREFIX="### "
+# ensure we have tmux available to us before we do anything else
+echo "$PREFIX ensure tmux is installed and available before we do anything"
+apt-get update
+apt-get install -y tmux
 # make sure there are no other sessions to get in the way
 echo "$PREFIX in-case there is an open tmux session, try to kill it"
 echo "$PREFIX this may error out, but that is ok"
